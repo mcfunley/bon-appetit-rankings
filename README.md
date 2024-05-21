33e941a7214b05b275cd8980dc46e52490e59d94
Dan McKinley <mcfunley@gmail.com>
2024-05-21 14:06:36 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Definitive Bon Appetit Test Kitchen ca. 2020 Classic Lineup Power Rankings, Visualized
+
+![The Definitive Bon Appetit Test Kitchen ca. 2020 Classic Lineup Power Rankings, Visualized](plot.png)
--- plot.py
+++ plot.py
@@ -1,63 +1,105 @@
 #!/usr/bin/env python
+import click
 import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d as mp3d
 import numpy as np
-import matplotlib.animation as animation
 
-# Example data points
 points = [
-    {'x': -5, 'y': 1, 'z': -1, 'label': 'Molly'},
-    {'x': -1, 'y': 4, 'z': 3, 'label': 'Claire'},
-    {'x': 1, 'y': 2, 'z': 4, 'label': 'Rick'},
-    {'x': 5, 'y': 5, 'z': 5, 'label': 'Sohla'},
-    {'x': 3, 'y': 0, 'z': 2, 'label': 'Gabby'},
-    {'x': -2, 'y': -2, 'z': -2, 'label': 'Andy'},
-    {'x': -5, 'y': -5, 'z': -5, 'label': 'Brad'},
-    {'x': -5, 'y': -5, 'z': 3, 'label': 'Delaney'},
-    {'x': 4, 'y': -4, 'z': -3, 'label': 'Chris'},
+    {'x': -5, 'y': 1, 'z': -1,
+     'label': 'Molly', 'ig_followers': 785,
+     },
+
+    {'x': -1, 'y': 4, 'z': 3,
+     'label': 'Claire', 'ig_followers': 1000,
+     },
+
+    {'x': 1, 'y': 2, 'z': 4,
+     'label': 'Rick', 'ig_followers': 234,
+     },
+
+    {'x': 5, 'y': 5, 'z': 5,
+     'label': 'Sohla', 'ig_followers':  581,
+     },
+
+    {'x': 3, 'y': 0, 'z': 2,
+     'label': 'Gabby', 'ig_followers': 237
+     },
+
+    {'x': -2, 'y': -2, 'z': -2,
+     'label': 'Andy', 'ig_followers':  333,
+     },
+
+    {'x': -5, 'y': -5, 'z': -5,
+     'label': 'Brad', 'ig_followers': 808,
+     },
+
+    {'x': -5, 'y': -5, 'z': 3,
+     'label': 'Delany', 'ig_followers': 272,
+     },
+
+    {'x': 4, 'y': -4, 'z': -3,
+     'label': 'Chris', 'ig_followers': 394,
+     },
+
+    {'x': 2, 'y': 4, 'z': 3,
+     'label': 'Carla', 'ig_followers': 410,
+     },
 ]
 
-# Create figure and 3D axes
-fig = plt.figure()
+minf = min([point['ig_followers'] for point in points])
+maxf = max([point['ig_followers'] for point in points])
+for p in points:
+    p['followers_norm'] = (p['ig_followers'] - minf) / (maxf - minf)
+
+S = 7
+
+fig = plt.figure(figsize=(8, 8))
 ax = fig.add_subplot(111, projection='3d')
+ax.xaxis.set_ticks([])
+ax.yaxis.set_ticks([])
+ax.zaxis.set_ticks([])
 
-# Plot points
+xy = np.array([
+    (-1, -1, 0),
+    (1, -1, 0),
+    (1,  1, 0),
+    (-1,  1, 0),
+]) * S
+plane = mp3d.art3d.Poly3DCollection([xy], alpha=0.1, linewidth=1)
+plane.set_facecolor((0, 0, 0))
+ax.add_collection(plane)
+
+ax.plot([-S, S], [0, 0], [0, 0], '--', alpha=0.2, color='k')
+ax.plot([0, 0], [-S, S], [0, 0], '--', alpha=0.2, color='k')
+ax.plot([0, 0], [0, 0], [-S, S], '--', alpha=0.2, color='k')
+
 for point in points:
-    ax.scatter(point['x'], point['y'], point['z'], label=point['label'])
-    ax.text(
-        point['x'] + 0.2,
-        point['y'] + 0.2,
-        point['z'] + 0.2,
-        point['label'])
-
-# Draw dotted black lines along the axes
-ax.plot([-5, 5], [0, 0], [0, 0], 'k--')  # X axis
-ax.plot([0, 0], [-5, 5], [0, 0], 'k--')  # Y axis
-ax.plot([0, 0], [0, 0], [-5, 5], 'k--')  # Z axis
+    x, y, z = point['x'], point['y'], point['z']
+    ax.scatter(
+        x, y, z,
+        label=point['label'],
+        s=10 + point['followers_norm'] * 100)
+    zoffs = -0.7 if z < 0 else 0.2
+    ax.text(x + 0.2, y + 0.2, z + zoffs, point['label'])
+    ax.plot([x, x], [y, y], [0, z], ':')
 
 x_label = ax.set_xlabel('influencer ⟸⟹ specialist')
 y_label = ax.set_ylabel('dumb ⟸⟹ smart')
 z_label = ax.set_zlabel('phony ⟸⟹ sincere')
 
-# Set limits to ensure origin is in the center
-ax.set_xlim([-5, 5])
-ax.set_ylim([-5, 5])
-ax.set_zlim([-5, 5])
+ax.set_xlim([-S, S])
+ax.set_ylim([-S, S])
+ax.set_zlim([-S, S])
 
 
-def rotate(angle):
-    ax.view_init(elev=10, azim=angle)
-    # Update axis labels positions
-    x_label.set_position((0.5, -0.1))
-    y_label.set_position((-0.1, 0.5))
-    z_label.set_position((0.1, 0.5))
-
-    x_label.set_rotation(angle)
-    y_label.set_rotation(angle)
-    z_label.set_rotation(angle)
+@click.command()
+@click.option('--save', is_flag=True, help='Save the plot to a file.')
+def main(save: bool = False):
+    if save:
+        plt.savefig('plot.png', dpi=1000)
+    else:
+        plt.show()
 
 
-ani = animation.FuncAnimation(
-    fig, rotate, frames=np.arange(0, 360, 1), interval=50)
-ani.save('ba-alignment.mp4', writer='ffmpeg', fps=20)
-
-plt.show()
+if __name__ == '__main__':
+    main()
